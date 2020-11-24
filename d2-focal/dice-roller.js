43e5746316bc6c4e1780db1f5c66bcfbaeb3bbc2
Robert <rbbenett0614@gmail.com>
2020-11-24 20:02:00 +0000

    Solution Written

--- d2-focal/dice-roller.js
+++ d2-focal/dice-roller.js
@@ -1,5 +1,14 @@
 const rollDice = function(number) {
-  
+  let rollArray = [];
+  for (let i = 0; i < number; i++) {
+    if (i < number - 1) {
+      rollArray += Math.ceil(Math.random() * Math.floor(6)) + ", ";
+    } else {
+      rollArray += Math.ceil(Math.random() * Math.floor(6))
+    }
+    
+  }
+  return `Rolled ${number} dice: ${rollArray}`
 }
 
 const diceNumber = process.argv.splice(2)
