6071adc89dece7a58fb73a50572f7ce7ab428b1a
Robert <rbbenett0614@gmail.com>
2020-12-03 15:08:40 +0000

    First Commit

--- d4-funcs/closures.js
+++ d4-funcs/closures.js
@@ -1,13 +1,12 @@
-function makeLoadedDice() {
+function makeLoadedDie() {
   const list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  /* your code here */
+  let i = 0;
   
-  } 
-
+  
   return function() {
-    /* your code here */
-    return item
-
+    let loadedRoll = list[i];
+    i++;
+    return loadedRoll;
   }
 }
 
@@ -16,3 +15,27 @@ const rollLoadedDie = makeLoadedDie();
 console.log(rollLoadedDie());  // 5
 console.log(rollLoadedDie());  // 4
 console.log(rollLoadedDie());  // 6
+
+const countdownGenerator = function (x) {
+  let timeToLiftoff = x;
+  return function() {
+    if(!timeToLiftoff) {
+      timeToLiftoff--;
+      return "Blast Off"
+    } 
+    if (timeToLiftoff < 0) {
+      timeToLiftoff--;
+      return "Rockets already gone, bub!";
+    }
+      timeToLiftoff--;
+      return `T-minus ${timeToLiftoff}...`
+  }
+};
+
+const countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
